802f5c0c451c4479c68c0d8e876f4dc13791b1e2
Lavínia Silva Dias <B46842@fgv.edu.br>
2024-11-22 08:45:08 -0300

    Crie o modelo drift
    Crie o modelo drift e otimize o seasonal Random Walk

--- models.py
+++ models.py
@@ -2,6 +2,9 @@ from abc import ABC, abstractmethod
 import numpy as np
 
 class Model(ABC):
+    """
+    Base class for models. All models must implement fit, predict, and num_params methods.
+    """
     @abstractmethod
     def fit(self, X, y):
         pass
@@ -12,7 +15,8 @@ class Model(ABC):
     
     @abstractmethod
     def num_params(self):
-        """Returns the number of parameters of the model.
+        """
+        Returns the number of parameters of the model.
         """
         pass
     
@@ -33,50 +37,64 @@ class Model(ABC):
         
         return report
 
-
 # Baseline models
 class Mean(Model):
+    """
+    Model that predicts the mean of the target variable.
+    """
     def fit(self, X, y):
-        self.mean = y.mean()
+        self.mean = np.mean(y)
 
-    def predict(self, x):
-        return np.full(len(x), self.mean)
+    def predict(self, X):
+        return np.full(len(X), self.mean)
     
     def num_params(self):
         return 1
 
 
 class RandomWalk(Model):
+    """
+    Model that predicts the last observed value of the target variable.
+    """
     def fit(self, X, y):
         self.last_value = y.iloc[-1]
-    def predict(self, x):
-        return np.full(len(x), self.last_value)
+
+    def predict(self, X):
+        return np.full(len(X), self.last_value)
+
     def num_params(self):
         return 1
 
 
 class SeasonalRandomWalk(Model):
-    def fit(self, X, y,seasonality):
+    """
+    Model that predicts based on seasonal patterns of the target variable.
+    """
+    def fit(self, X, y, seasonality):
         self.seasonality = seasonality
-        self.last_season = y.iloc[-seasonality:]
-        self.season_orientation = X.iloc[-seasonality:]['Quarter'] % seasonality
+        self.last_observation = X.iloc[-1]
+        self.last_season = y.iloc[-seasonality:].values
     def predict(self, x):
-        quarters = x['Quarter']
-        quarters = quarters % self.seasonality
-        # Print the position index for the values of quarters in the season_orientation
-        predictions_index = np.array([np.where(self.season_orientation == quarter)[0][0] for quarter in quarters])
-        predictions = []
-        for i in range(len(predictions_index)):
-            location = predictions_index[i]
-            predictions.append(self.last_season.iloc[location])
-        return predictions
+        h = x['Quarter'] - self.last_observation['Quarter']
+        k = np.floor((h - 1) / self.seasonality).astype(int)
+        indices = (h - self.seasonality * (k + 1)) -1
+        return self.last_season[indices]
+
     def num_params(self):
         return self.seasonality
 
 
 
 class Drift(Model):
-    ...
+    def fit(self, X, y):
+        self.C = (y.iloc[-1] - y.iloc[0]) / (len(y)-1)
+        self.last_value = y.iloc[-1]
+        self.last_observation = X.iloc[-1]
+    def predict(self, x):
+        self.h = x['Quarter'] - self.last_observation['Quarter']
+        return self.last_value + self.C * self.h
+    def num_params(self):
+        return 2
 
 
 class ExponentialSmoothing(Model):
@@ -85,10 +103,12 @@ class ExponentialSmoothing(Model):
 
 class LinearRegression(Model):
     def fit(self, X, y):
+        X = X.drop(columns=["Date"])
         X = np.c_[np.ones(X.shape[0]), X.values]
         self.coefficients = np.linalg.pinv(X.T @ X) @ X.T @ y.values
 
     def predict(self, x):
+        x = x.drop(columns=["Date"])
         x = np.c_[np.ones(x.shape[0]), x.values]  
         return x @ self.coefficients
     
