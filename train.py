05d107db4542b37cb6cd777d526ce3db01d7e1b0
Lavínia Silva Dias <B46842@fgv.edu.br>
2024-11-22 10:41:55 -0300

    Estime a tendência dos dados

--- train.py
+++ train.py
@@ -4,7 +4,14 @@ import pandas as pd
 
 
 def main():
-    data = pd.read_csv("us_change.csv")
+    data = pd.read_csv("us_change_cleaned.csv")
+    X = data.drop(columns='Consumption')
+    y = data['Consumption']
+
+    X_train = X[:int(0.8*len(X))]
+    y_train = y[:int(0.8*len(y))]
+    X_test = X[int(0.8*len(X)):]
+    y_test = y[int(0.8*len(y)):]
     
     models_list = [
         models.Mean(),
@@ -23,7 +30,12 @@ def main():
     ]
     
     for model in models_list:
-        model.fit(...)
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        metrics = {}
+        for metric in metrics_list:
+            metrics[metric.__name__] = metric(y_test, y_pred)
+        
         
 
 
