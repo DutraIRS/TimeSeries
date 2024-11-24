dde2db9f9c06f363b1475b79d1e7f50a6c408be4
Iago Riveiro Santos Dutra <iagorivsd@gmail.com>
2024-11-23 21:58:16 -0300

    generic metrics always accept more args
    useful to write a loop that always passes the number of parameters, the metric decides if wants to use it or not

--- metrics.py
+++ metrics.py
@@ -1,12 +1,15 @@
 import numpy as np
 
-def rmse(y_true, y_pred):
+def rmse(y_true, y_pred, *args):
     return np.sqrt(np.mean((y_true - y_pred) ** 2))
 
-def mae(y_true, y_pred):
+def mae(y_true, y_pred, *args):
     return np.mean(np.abs(y_true - y_pred))
 
-def r_squared(y_true, y_pred):
+def mape(y_true, y_pred, *args):
+    return np.mean(np.abs((y_true - y_pred) / y_true))
+
+def r_squared(y_true, y_pred, *args):
     ss_res = np.sum((y_true - y_pred) ** 2)
     ss_tot = np.sum((y_true - np.mean(y_true)) ** 2)
     
@@ -17,13 +20,13 @@ def aic(y_true, y_pred, num_params):
     return -2*np.log(rss) + 2*num_params
 
 def aic_corrected(y_true, y_pred, num_params):
-    aic = aic(y_true, y_pred, num_params)
+    aic_ = aic(y_true, y_pred, num_params)
     n = len(y_true)
     
-    return aic + 2*num_params*(num_params + 1)/(n - num_params - 1)
+    return aic_ + 2*num_params*(num_params + 1)/(n - num_params - 1)
 
 def bic(y_true, y_pred, num_params):
-    aic = aic(y_true, y_pred, num_params)
+    aic_ = aic(y_true, y_pred, num_params)
     n = len(y_true)
     
-    return aic + num_params*(np.log(n) - 2)
+    return aic_ + num_params*(np.log(n) - 2)
